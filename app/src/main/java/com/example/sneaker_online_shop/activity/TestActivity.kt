0a99d127f4df913185d97b0397808c53caecb514
DzungNguyen513 <nguyentrungdung0501@gmail.com>
2024-07-22 17:22:47 +0700

    first commit

---
+++ app/src/main/java/com/example/sneaker_online_shop/activity/TestActivity.kt
@@ -0,0 +1,21 @@
+package com.example.sneaker_online_shop.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sneaker_online_shop.R
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_base)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
